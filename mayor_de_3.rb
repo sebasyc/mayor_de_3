c0293884c40bfb9949fcf4cf5fd7f3b1584d59d5
Sebastian Campos <pseba@outlook.com>
2020-09-01 18:05:15 -0400

    refactorizacion

--- mayor_de_3.rb
+++ mayor_de_3.rb
@@ -2,15 +2,10 @@ num1 = ARGV[0].to_i
 num2 = ARGV[1].to_i
 num3 = ARGV[2].to_i
 
-
-if num1 > num2
-    if num1 > num3
-        puts num1
-    else
-        puts num3
-    end
-elsif num2 > num3
-    puts num2
+if num1 >= num2 && num1 >= num3
+    puts num1
+    elsif num2 >= num1 && num2 >= num3
+        puts num2
     else
         puts num3
 end
